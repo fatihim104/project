6dc7feefb46c93fd115a94dde0ef46c50e10e4ca
Fatih imal <fatihimal@gmail.com>
2020-11-29 17:18:09 +0000

    Horoscope homework added.

---
+++ javascript/horoscopes/scripts.js
@@ -0,0 +1,114 @@
+//Basla
+let ay = "Ekim";
+let gun = 14;
+
+if(ay=="Ocak"){
+    if(gun>0 && gun<=31 && gun>=22){
+        console.log("Kova Burcu")   
+    }else if(gun>0 && gun<=31 && gun<22){
+        console.log("Oglak Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+}else if(ay=="Subat"){
+    if(gun>0 && gun<=29 && gun>=20){
+        console.log("Balik Burcu")
+    }else if(gun>0 && gun<=29 && gun<20){
+        console.log("Kova Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Mart"){
+    if(gun>0 && gun<=31 && gun>=21){
+        console.log("Koc Burcu")
+    }else if(gun>0 && gun<=31 && gun<21){
+        console.log("Balik Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Nisan"){
+    if(gun>0 && gun<=30 && gun>=21){
+        console.log("Boga Burcu")
+    }else if(gun>0 && gun<=30 && gun<21){
+        console.log("Koc Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Mayis"){
+    if(gun>0 && gun<=31 && gun>=22){
+        console.log("Ikizle Burcu")
+    }else if(gun>0 && gun<=31 && gun<22){
+        console.log("Boga Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Haziran"){
+    if(gun>0 && gun<=30 && gun>=23){
+        console.log("Yengec Burcu")
+    }else if(gun>0 && gun<=30 && gun<23){
+        console.log("Ikizler Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Temmuz"){
+    if(gun>0 && gun<=31 && gun>=23){
+        console.log("Aslan Burcu")
+    }else if(gun>0 && gun<=31 && gun<23){
+        console.log("Yengec Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Agustos"){
+    if(gun>0 && gun<=31 && gun>=23){
+        console.log("Basak Burcu")
+    }else if(gun>0 && gun<=31 && gun<23){
+        console.log("Aslan Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Eylül"){
+    if(gun>0 && gun<=30 && gun>=23){
+        console.log("Terazi Burcu")
+    }else if(gun>0 && gun<=30 && gun<23){
+        console.log("Basak Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Ekim"){
+    if(gun>0 && gun<=31 && gun>=23){
+        console.log("Akrep Burcu")
+    }else if(gun>0 && gun<=31 && gun<23){
+        console.log("Terazi Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }
+
+}else if(ay=="Kasim"){
+    if(gun>0 && gun<=30 && gun>=22){
+        console.log("Yay Burcu")
+    }else if(gun>0 && gun<=30 && gun<22){
+        console.log("Akrep Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }    
+
+}else if(ay=="Aralik"){
+    if(gun>0 && gun<=31 && gun>=22){
+        console.log("Oglak Burcu")
+    }else if(gun>0 && gun<=31 && gun<22){
+        console.log("YAY Burcu")
+    }else{
+        console.log("Iyimisin?Böyle bir tarih yoktur!!")
+    }  
+}else{
+    console.log("Ayi yanlis yazdiniz!!Yalnizca basharfi büyük olsun ve Türkce karakter kullanmayiniz!!!")
+}
+//Bitir.
